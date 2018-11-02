1431b89df6bc0d775e2a4d074d57c37599b1499b
lynnjepsen <lynnjepsen@google.com>
2018-11-02 15:33:17 -0700

    include MDC React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Button from '@material/react-button/dist'; // /index.js is implied
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Button
+          raised
+          className='button-alternate'
+          onClick={() => console.log('clicked!')}
+        >
+          Click Me!
+        </Button>
       </div>
     );
   }
